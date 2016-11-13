6fe30511913e6336fa483a0e98560dba21317dcf
Jannik Wibker <jannikwibker@gmail.com>
2016-11-13 22:08:22 +0100

    switched to "extends Class" from "extends React.Component"

--- components/Header.js
+++ components/Header.js
@@ -7,7 +7,7 @@ import event from '../util/event.js'
 import Class from './Class.js'
 
 
-export default class Header extends React.Component {
+export default class Header extends Class {
 
   constructor(){
     super()
@@ -19,44 +19,10 @@ export default class Header extends React.Component {
 
     this.update_css = this.update_css.bind(this)
     this.css = this.css.bind(this)
+    this.theme_event = this.theme_event.bind(this)
 
-    event.subscribe('theme', (e) => {
-
-      // function to swap theme
-      let swap = theme => {
-        if(theme.name === dark_theme.name) {
-          return light_theme
-        } else if(theme.name === light_theme.name) {
-          return dark_theme
-        }
-      }
-
-      this.theme = swap(this.theme)
-      this.css()
-      if(this._mounted) this.forceUpdate()
-    })
-
+    this.theme_event()
     this.update_css()
-  }
-  // componentWillMount
-  componentWillMount() {
-    this._mounted = true
-    this.update_css()
-  }
-  // componentWillUnmount
-  componentWillUnmount() {
-    this._mounted = false
-  }
-
-  // update_css (set the theme given via props or default to light theme)
-  update_css() {
-    if(this.props && this.props.theme) {
-      this.theme = this.props.theme
-    } else {
-      this.theme = light_theme
-    }
-
-    this.css()
   }
   // css (set all the css using glamor (aliased to css because next.js) with the current theme)
   css() {
