58868b939de8bf22a89ec5ae8716d392c2026738
Jannik Wibker <jannikwibker@gmail.com>
2016-11-14 07:40:03 +0100

    changed accentColor a bit (now more visible)

--- config/themes.js
+++ config/themes.js
@@ -4,14 +4,14 @@ const dark_theme =  {
   backgroundColor: '#000',
   color: '#fff',
   invertColor: '#000',
-  accentColor: '#444'
+  accentColor: '#666'
 }
 const light_theme = {
   name: 'light_theme',
   backgroundColor: '#fff',
   color: '#222',
   invertColor: '#fff',
-  accentColor: '#ccc'
+  accentColor: '#aaa'
 }
 
 export  { dark_theme, light_theme }
