848d9918fd898b780e895083aabc707b321a6209
Jannik Wibker <jannikwibker@gmail.com>
2016-11-20 22:15:17 +0100

    changed arguments

--- util/gen_hash.js
+++ util/gen_hash.js
@@ -1,4 +1,4 @@
-let gen_hash = (node, path, name, length = 16) => {
+let gen_hash = (length = 16) => {
   let n = new Date().valueOf().toString(), result = '', p = 0, chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
   for(let i = length; i > 0; --i) {
     result += ((i & 1) && n.charAt(p) ? n.charAt(p) : chars[Math.floor(Math.random() * chars.length)])
