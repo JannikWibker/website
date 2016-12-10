74c6938866d29c8b7756123c94ff25e2576e9553
Jannik Wibker <jannikwibker@gmail.com>
2016-12-10 16:54:59 +0100

    login system v0.0.2 and linkColor now part of themes

--- components/Login.js
+++ components/Login.js
@@ -21,6 +21,7 @@ export default class Block extends Class {
 
   css() {
     this.style__login_input = css({
+      "backgroundColor": this.theme.backgroundColor + "!important",
       "padding": "0px 10px 5px 10px!important",
       "marginBottom": "20px",
       "fontSize": "12px",
@@ -42,24 +43,31 @@ export default class Block extends Class {
     })
 
     this.style__login_button = css({
-
+      "width": "128",
+      "height": "24px",
+      "margin": "0 10px 0 94px",
+      "cursor": "pointer",
+      "color": this.theme.color,
+      ":hover": {
+        "color": this.theme.linkColor
+      }
     })
   }
 
   check() {
-
+    
   }
 
   render() {
     return (
       <div className={""} >
         <span className={this.style__login_span} >username</span>
-        <input type="text" placeholder="username" className={this.style__login_input}></input><br />
+        <input type="text" placeholder="username" className={this.style__login_input} ref={(input) => {this.username = input}}></input><br />
         <span className={this.style__login_span} >email</span>
-        <input type="text" placeholder="you@mailprovider.com" className={this.style__login_input}></input><br />
+        <input type="text" placeholder="you@mailprovider.com" className={this.style__login_input} ref={(input) => {this.email = input}}></input><br />
         <span className={this.style__login_span} >password</span>
-        <input type="password" placeholder="your p4ssw0rd" className={this.style__login_input}></input>
-        <div className={this.style__login_button}>login</div>
+        <input type="password" placeholder="your p4ssw0rd" className={this.style__login_input} ref={(input) => {this.password = input}}></input>
+        <div className={this.style__login_button} onClick={this.check}>login</div>
       </div>
     )
   }
--- config/themes.js
+++ config/themes.js
@@ -4,21 +4,24 @@ const dark_theme =  {
   backgroundColor: '#000',
   color: '#fff',
   invertColor: '#000',
-  accentColor: '#666'
+  accentColor: '#666',
+  linkColor: 'rgba(248,28,229,0.75)'
 }
 const light_theme = {
   name: 'light_theme',
   backgroundColor: '#fff',
   color: '#222',
   invertColor: '#fff',
-  accentColor: '#aaa'
+  accentColor: '#aaa',
+  linkColor: 'rgba(248,28,229,0.75)'
 }
 const purple_theme= {
   name: 'purple_theme',
   backgroundColor: '#180018',
   color: '#BAA6BF',
   invertColor: '#180018 ',
-  accentColor: '#280B29'
+  accentColor: '#280B29',
+  linkColor: 'rgba(248,28,229,0.75)'
 }
 
 export  { dark_theme, light_theme, purple_theme }
