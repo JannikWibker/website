c51cbe20d9ec15c7372ae01415d23990b4f63a15
Jannik Wibker <jannikwibker@gmail.com>
2016-12-30 13:51:38 +0100

    minor changes and updated to Next.js 1.2.3

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -4,6 +4,7 @@ import Header from '../components/Header.js'
 import Content from '../components/Content.js'
 import Block from '../components/Block.js'
 import Footer from '../components/Footer.js'
+import AccountName from '../components/AccountName.js'
 import { dark_theme, light_theme, purple_theme, blue_theme } from '../config/themes.js'
 import event_loader from '../util/event_loader.js'
 export default class DashboardPage extends React.Component {
@@ -11,10 +12,12 @@ export default class DashboardPage extends React.Component {
   constructor(props) {
     super(props)
   }
+
   componentWillMount() {
     event_loader(['theme', 'account'])
     // loading events (see /util/event_loader.js for additional information)
   }
+  
   render() {
     event_loader(['theme', 'account'])
 
@@ -23,9 +26,12 @@ export default class DashboardPage extends React.Component {
       <div className="root">
         <Globals />
         <Header theme={light_theme}/>
-        <Content>
+        <Content theme={light_theme}>
           <Block theme={light_theme}>
-            Dashboard - Account stuff goes here
+            <div>
+              Dashboard - Account stuff goes here<br />
+              <AccountName />
+            </div>
           </Block>
         </Content>
         <Footer theme={light_theme}/>
