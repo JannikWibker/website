d3bd7490ab9e0c28add55d24ce6ec46b3e520db1
Jannik Wibker <jannikwibker@gmail.com>
2017-03-04 11:14:46 +0100

    removed Script tag on index page (the programming language thingy)

--- pages/index.js
+++ pages/index.js
@@ -56,34 +56,6 @@ export default class IndexPage extends React.Component {
             </Keyframes>
             </Terminal>
           </Block>
-          <Script html={`
-            <textarea id="input">1+1</textarea>
-            <div id="btn">
-              <button id="run">run</button><br>
-              <button id="clear">clear</button>
-            </div>
-            <style>
-              #input{
-                height:100%;
-                width: 156px;
-              }
-              #btn{
-                float: right;
-              }
-              #run,#clear{
-                width: 42px;
-              }</style>`} scripts={[
-            'url:./static/js/browser.min.js',
-            `window.addEventListener('_load', () => {
-              document.getElementById('run').addEventListener('click', () => {
-                console.log(Lambda(document.getElementById('input').value))
-              })
-              document.getElementById('clear').addEventListener('click', () => {
-                console.clear()
-              })
-            })`
-                  ]}/>
-
         </Content>
         <Footer theme={dark_theme} />
       </div>
