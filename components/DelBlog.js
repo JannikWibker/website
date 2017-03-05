48c447a62f0ee252b3022a3b0edadd884d220465
Jannik Wibker <jannikwibker@gmail.com>
2017-03-05 12:28:08 +0100

    added DelBlog component (currently only lists all blogs (only title / name) and is not able to delete them (will change soon))

---
+++ components/DelBlog.js
@@ -0,0 +1,53 @@
+import React from 'react'
+import css from 'next/css'
+import post from '../util/post.js'
+import { dark_theme, light_theme } from '../config/themes.js'
+import Class from './Class.js'
+import Button from './Button.js'
+import event from '../util/event.js'
+
+export default class DelBlog extends Class {
+  constructor(props) {
+    super(props)
+
+    this.posts = []
+
+    this.get()
+  }
+
+  css() {
+
+  }
+
+  get() {
+    if(typeof window === 'undefined') {
+      return {}
+    }
+    fetch(`http://${location.hostname}:3001/blog/get`)
+      .then((data) => {
+        return data.json()
+      })
+      .then((json) => {
+        this.posts = Object.keys(json).map(key => json[key])
+        this.posts.sort((a, b) => {
+          return new Date(b.createdAt) - new Date(a.createdAt)
+        })
+        if(this._mounted && typeof window !== 'undefined') this.forceUpdate()
+      })
+  }
+
+  render() {
+    return (
+      <div>
+        <ul style={{textAlign: 'left'}}>
+        {this.posts.map((x, i) => (
+          <li key={i}>
+            <span>{x.name}</span>
+            <span> X</span>
+          </li>
+        ))}
+        </ul>
+      </div>
+    )
+  }
+}
