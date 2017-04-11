e6d58777d5bd6b8db4a16564ccbacbc21f9776a6
Jannik Wibker <jannikwibker@gmail.com>
2017-04-11 23:08:49 +0200

    Component displaying an error (used for Browser window stuff)

---
+++ components/Custom/ErrorComponent.js
@@ -0,0 +1,25 @@
+export default ({id = 404, text = 'This page could not be found'}) => (
+  <div style={{
+    textAlign: 'center',
+    paddingTop: '20%',
+    fontFamily: '-apple-system, "SF UI Text", "Helvetica Neue", "Lucida Grande", sans-serif'}}>
+    <h1 style={{
+      display: 'inline-block',
+      marginRight: '20px',
+      padding: '10px 23px',
+      fontWeight: 500,
+      fontSize: '24px',
+      borderRight: '1px solid rgba(0, 0, 0,.3)'
+    }}>{id}</h1>
+    <div style={{
+      display: 'inline-block',
+      lineHeight: '49px'
+    }}>
+      <h2 style={{
+        letterSpacing: '0.1px',
+        fontSize: '14px',
+        fontWeight: 'normal'
+      }}>{text}</h2>
+    </div>
+  </div>
+)
