cf9d5850f394d2acc4a2d50ebaede81c2275d4e2
Jannik Wibker <jannikwibker@gmail.com>
2017-04-11 16:44:23 +0200

    see new code comments

--- components/HTML.js
+++ components/HTML.js
@@ -1,6 +1,15 @@
 import React from 'react'
 export default props => props.StringToHTML
-  ? <div dangerouslySetInnerHTML={{__html: props.children.replace(/\n/g, '<br />').replace(/\^/g, '&nbsp;') }} />
+  ? <div dangerouslySetInnerHTML={{__html: props.children
+          .replace(/\n/g, '<br />')
+          .replace(/\^/g, '&nbsp;')
+          .replace(/≤1/g, '<span style="color: #2aa198">')
+          .replace(/≤2/g, '<span style="color: #b58900">')
+          .replace(/≤3/g, '<span style="color: #dc322f">')
+          .replace(/≤4/g, '<span style="color: #d33682">')
+          .replace(/≤5/g, '<span style="color: #cb4b16">')
+          .replace(/≥/g, '</span>')
+         }} />
   : <div dangerouslySetInnerHTML={{__html: props.children }} />
 /*
 component which turns children to InnerHTML
@@ -9,4 +18,11 @@ is really long and annoying to type. This is also
 used in some cases because it fixes some errors with
 components that do some manipulation with their children
 (innerHTML does not really work with that))
+
+bool StringToHTML (true | false):
+if true then:
+every newline is replaced by a <br />,
+every ^ (caret) is replaced by a &nbsp; (because replacing whitespace directly is annoying),
+every ≤[1-5] is replaced by <span style="color: #[colors 1-5]">,
+every ≥ is replaced by </span>
 */
