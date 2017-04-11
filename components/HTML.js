c3297347d4d7e7527fd87f78d751efcb192e2f55
Jannik Wibker <jannikwibker@gmail.com>
2017-04-11 16:48:55 +0200

    added comments explaining what each color should be used for

--- components/HTML.js
+++ components/HTML.js
@@ -3,11 +3,11 @@ export default props => props.StringToHTML
   ? <div dangerouslySetInnerHTML={{__html: props.children
           .replace(/\n/g, '<br />')
           .replace(/\^/g, '&nbsp;')
-          .replace(/≤1/g, '<span style="color: #2aa198">')
-          .replace(/≤2/g, '<span style="color: #b58900">')
-          .replace(/≤3/g, '<span style="color: #dc322f">')
-          .replace(/≤4/g, '<span style="color: #d33682">')
-          .replace(/≤5/g, '<span style="color: #cb4b16">')
+          .replace(/≤1/g, '<span style="color: #2aa198">') // variables
+          .replace(/≤2/g, '<span style="color: #b58900">') // strings
+          .replace(/≤3/g, '<span style="color: #dc322f">') // object keys
+          .replace(/≤4/g, '<span style="color: #d33682">') // methods / functions
+          .replace(/≤5/g, '<span style="color: #cb4b16">') // jsx html tags
           .replace(/≥/g, '</span>')
          }} />
   : <div dangerouslySetInnerHTML={{__html: props.children }} />
