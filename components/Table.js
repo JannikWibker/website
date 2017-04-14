cebaeefa647fadd15c3e19be58586b96ab7c4a6e
Jannik Wibker <jannikwibker@gmail.com>
2017-04-14 15:56:22 +0200

    added Table component (features: - renders table - able to inject css using props - swap between x/y and y/x layout (bad naming but pretty much like swapping x and y on a graph) )

---
+++ components/Table.js
@@ -0,0 +1,57 @@
+import React from 'react'
+import css from 'next/css'
+
+export default ({data = [], layout = true, fill = false, style = {}}) => {
+
+  const css__name = {
+    fontWeight: 'bold'
+  }
+
+  let range_from_data = data => [...Array(data.sort((x,y)=>x.cells.length-y.cells.length).reverse()[0].cells.length).keys()]
+
+  let cell = (c, j, k) => (
+    <td className={css(style.cell) || ''} id={j + '.' + (k+1)} key={j + '.' + (k+1)}>
+      {c}
+    </td>
+  )
+
+  let name = (c, j) => (
+    <td className={css(style.name || css__name || {})} id={j + '.0'} key={j + '.0'}>
+      {c}
+    </td>
+  )
+
+  let rows = data => (
+    <tbody>
+      {data.map((d, j) => (
+        <tr className={css(style.tr) || ''} key={j}>
+          {d.name !== '' ? name(d.name, j) : ''}
+          {fill
+            ? range_from_data([...data]).map(k =>
+                cell(d.cells[k] || '', j, k))
+            : d.cells.map((c, k) =>
+                cell(d.cells[k], j, k))
+          }
+        </tr>
+      ))}
+    </tbody>)
+
+  let columns = (data) => (
+    <tbody>
+      <tr className={css(style.tr) || ''} key={0}>
+        {data.map((c, i) => name(c.name, i))}
+      </tr>
+      {range_from_data([...data]).map(i => (
+        <tr className={css(style.tr) || ''} key={i+1}>
+          {data.map((c, j) => cell(c.cells[i], i+1, j) )}
+        </tr>
+      ))}
+    </tbody>
+  )
+
+  return (
+    <table className={css(style.table)}>
+        {layout ? rows(data) : columns(data)}
+    </table>
+  )
+}
